dabbd6edf170639552b534d180918603ff971d53
John <john@anatec.io>
2017-05-10 15:13:49 +0200

    Adds level1

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,14 @@
-require "json"
+require 'json'
+require 'date'
 
-# your code
+data = JSON.parse(File.read(__dir__ + '/data.json'))
+cars = data['cars']
+rentals = data['rentals'].map do |rental|
+  car = cars.find { |car| car['id'] == rental['car_id'] }
+  period = (Date.parse(rental['end_date']) - Date.parse(rental['start_date'])).to_i + 1
+  price = (car['price_per_day'] * period) + (car['price_per_km'] * rental['distance'])
+
+  { id: rental['id'], price: price }
+end
+
+File.write(__dir__ + '/output.json', JSON.pretty_generate(rentals: rentals))
